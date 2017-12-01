c81686934534186f060022cdbfe53c2e696b7a2c
Tomasz Galecki <tomasz.galecki@hotmail.com>
2017-12-01 16:46:23 +0100

    add shaders compilation

--- GKOM-opengl/main.cpp
+++ GKOM-opengl/main.cpp
@@ -9,9 +9,10 @@ int main()
 	// RENDER LOOP
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
-
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		render();
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -60,4 +61,104 @@ void initGlad() {
 void processInput(GLFWwindow *window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+}
+
+void render() {
+	unsigned VBO = verticesPrepare();
+	//unsigned VAO = attributesPrepare();
+	unsigned shaderProgram = uploadShaderOnGpu();
+
+}
+
+unsigned verticesPrepare() {
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+	unsigned VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	return VBO;
+}
+
+unsigned attributesPrepare() {
+	return 0;
+}
+
+unsigned uploadShaderOnGpu() {
+	unsigned vertexShader = vertexShaderPrepare();
+	unsigned fragmentShader = fragmentShaderPrepare();
+	unsigned shaderProgram = shaderProgramPrepare(vertexShader, fragmentShader);
+	glUseProgram(shaderProgram);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	return shaderProgram;
+}
+
+unsigned vertexShaderPrepare() {
+	const char *vertexShaderSource =
+		"#version 330 core\n"
+		"layout(location = 0) in vec3 aPos;\n"
+		"void main(){\n"
+		"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\n";
+	unsigned vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	checkShaderCompilation(vertexShader, "VERTEX");
+	return vertexShader;
+}
+
+void checkShaderCompilation(unsigned shader, const char *shaderType) {
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::" << shaderType << "::COMPILATION_FAILED\n" << infoLog << std::endl;
+		glfwTerminate();
+		exit(-1);
+	}
+}
+
+unsigned fragmentShaderPrepare() {
+	const char *fragmentShaderSource =
+		"#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main() {\n"
+		"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); \n"
+		"}\n";
+	unsigned fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	checkShaderCompilation(fragmentShader, "FRAGMENT");
+	return fragmentShader;
+}
+
+unsigned shaderProgramPrepare(unsigned vertexShader, unsigned fragmentShader) {
+	unsigned shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	checkShaderProgramCompilation(shaderProgram);
+	return shaderProgram;
+}
+
+void checkShaderProgramCompilation(unsigned shaderProgram) {
+	int  success;
+	char infoLog[512];
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::LINK_FAILED\n" << infoLog << std::endl;
+		glfwTerminate();
+		exit(-1);
+	}
 }
--- GKOM-opengl/main.h
+++ GKOM-opengl/main.h
@@ -11,4 +11,15 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void initGlad();
 void processInput(GLFWwindow *window);
 
+void render();
+unsigned verticesPrepare();
+unsigned attributesPrepare();
+
+unsigned uploadShaderOnGpu();
+unsigned vertexShaderPrepare();
+void checkShaderCompilation(unsigned shader, const char *shaderType);
+unsigned fragmentShaderPrepare();
+unsigned shaderProgramPrepare(unsigned vertexShader, unsigned fragmentShader);
+void checkShaderProgramCompilation(unsigned shaderProgram);
+
 #endif // MAIN_H
