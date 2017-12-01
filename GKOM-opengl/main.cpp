947b3572414e9428f50426b33e61a43d716b446f
Tomasz Galecki <tomasz.galecki@hotmail.com>
2017-12-01 15:14:29 +0100

    add initial window with resize

--- GKOM-opengl/main.cpp
+++ GKOM-opengl/main.cpp
@@ -1,5 +1,56 @@
-#include <glad\glad.h>
+#include "main.h"
 
-int main() {
-	GLclampf lol;
+int main()
+{
+	std::cout << "Hello" << std::endl;
+	initOpengl();
+	GLFWwindow* window = createWindow();
+	
+	initGlad();
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void initOpengl() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+GLFWwindow *createWindow() {
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Tomasz Galecki OPENGL project", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		exit(-1);
+	}
+	// set resize callback
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	return window;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	// resize callback
+	glViewport(0, 0, width, height);
+}
+
+void initGlad() {
+	// init glad
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		glfwTerminate();
+		exit(-1);
+	}
+
 }
---
+++ GKOM-opengl/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void initOpengl();
+GLFWwindow *createWindow();
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void initGlad();
+
+#endif // MAIN_H
